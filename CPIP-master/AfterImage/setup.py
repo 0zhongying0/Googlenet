2de0302a6b2c3bbe5517bb75519cba185e29d6b6
YuexuanX <chenyifan0127@outlook.com>
2018-09-26 21:28:38 +0800

    remove

--- CPIP-master/AfterImage/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-# from Cython.Build import cythonize
-import Cython
-from Cython import Build
-setup(
-    ext_modules = Cython.Build.cythonize(["AfterImage/*.pyx"])
-)
